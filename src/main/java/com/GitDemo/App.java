5344ff0740582dbbfa26a437cf02deca21e50aad
araya <arieman40@gmail.com>
2025-04-21 10:19:24 +0300

    demo is added to local

---
+++ src/main/java/com/GitDemo/App.java
@@ -0,0 +1,18 @@
+package com.GitDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	
+	public void addData()
+	{
+		System.out.println("data has been added");
+	}
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
